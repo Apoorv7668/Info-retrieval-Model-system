03a5cc7013079ba5630c4602f3f6b8e0b0b09400
Apoorv7668 <apoorvpanwar2333@gmail.com>
2025-03-16 01:38:10 +0530

    requirements_added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AI Project',
+    version= '0.0.0',
+    author= 'Apoorv ',
+    author_email= 'apoorvpanwar2333@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
